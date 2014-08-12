8222776d6bf01ceec5902316f21003fc48aa4a45
Elango Palanisamy <ela.ece76@gmail.com>
2014-08-12 13:04:04 +0530

    Bmp 1 byte print from command line is sucsess

---
+++ bmp.h
@@ -0,0 +1,20 @@
+
+extern int g[2000];
+extern u8 tmp[48];
+
+bmp_conv()
+{
+  
+
+
+if(g[4]==65 && g[5]==65)           // 
+{
+
+tmp[0]=0xaa;
+
+}
+
+printk(KERN_ALERT "in funct....%d \n",tmp[0]);
+
+
+}
